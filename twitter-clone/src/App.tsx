35098eb8f2340747417c39de8d5eba7e996eb91c
Taras Chuiko <tchuiko@griddynamics.com>
2022-08-11 10:58:17 +0300

    Added routing, fixed TS errors, added eslint

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ twitter-clone/src/components/LogIn/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function LogIn() {
+  return <h1>Log In</h1>;
+}
